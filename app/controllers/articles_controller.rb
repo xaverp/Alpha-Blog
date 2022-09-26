de21dea885cca909711417ee0d33d652c160ba2f
xaverp <xaverp@iiw.org>
2022-09-26 13:59:34 +0000

    new article path and params created

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,16 @@ class ArticlesController < ApplicationController
         @articles = Article.all
     end
 
+    def new
+    
+    end
+
+    def create
+        #render plain:  params[:article] - USE THIS TO RENDER FORM RESPONSE AS LOCAL HTML
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        #render plain: @article.inspect - USE THIS TO RENDER THE ARTICLE WITH ID AND ATTRIBUTES AS AN OBJECT
+        @article.save
+        redirect_to @article
+    end
+
 end 
