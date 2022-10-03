0678ddeae69f10621e93c29714c5f35e27573565
xaverp <xaverp@iiw.org>
2022-10-03 20:43:30 +0000

    Added user authentication methods and links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :edit, :update] #THIS WILL RUN THE PRIVATE METHOD "SET_USERS" ONLY ON SHOW, EDIT AND UPDATE
     
     def show
-        @user = User.find(params[:id])
         @articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
 
@@ -14,11 +14,9 @@ class UsersController < ApplicationController
     end
 
     def edit
-        @user = User.find(params[:id])
     end
 
     def update
-        @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:notice] = "Your account information was successfully updated"
             redirect_to @user
@@ -30,6 +28,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save #IF USER WAS SAVED GO TO NEXT LINE
+            session[:user_id] = @user.id #THIS SIGNS A USER IN ONCE THEY SIGN UP
             flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
             redirect_to articles_path
         else #ELSE RE-RENDER THE NEW USER FORM
@@ -40,5 +39,9 @@ class UsersController < ApplicationController
     private
     def user_params
         params.require(:user).permit(:username, :email, :password)
+    end
+
+    def set_user
+        @user = User.find(params[:id])
     end
 end
