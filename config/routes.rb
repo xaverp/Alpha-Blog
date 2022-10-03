93be61ae829c2531410ee99f22a3380223f9f7d8
xaverp <xaverp@iiw.org>
2022-10-03 18:16:18 +0000

    Added login form and routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   resources :articles
   get 'signup', to: 'users#new'  #here is the path name going to the action, signup_path, new action inside of users
   resources :users, except: [:new]
+  get 'login', to: 'sessions#new' #SESSIONS CONTROLLER, NEW ACTION
+  post 'login', to: 'sessions#create' # CREATE ACTION 
+  delete 'logout', to: 'sessions#destroy' #DESTROY ACTION TO END SESSION
 end
