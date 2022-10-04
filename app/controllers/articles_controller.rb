fad318f502d488820317b8f69c72d740616f89fd
xaverp <xaverp@iiw.org>
2022-10-04 20:36:27 +0000

    Added admin title and delete buttons for profiles and articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -55,7 +55,7 @@ class ArticlesController < ApplicationController
     end
 
     def require_same_user
-        if current_user != @article.user
+        if current_user != @article.user && !current_user.admin?
             flash[:alert] = "Edit permissions denied"
             redirect_to @article
         end    
