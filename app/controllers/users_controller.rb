fbe0ab643f3a1a4ac918726752b56cfc05a27c07
xaverp <xaverp@iiw.org>
2022-10-04 15:24:26 +0000

    Added user auth restriction

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:show, :edit, :update] #THIS WILL RUN THE PRIVATE METHOD "SET_USERS" ONLY ON SHOW, EDIT AND UPDATE
+    before_action :require_user, only: [:edit, :update]
+    before_action :require_same_user, only: [:edit, :update]
     
     def show
         @articles = @user.articles.paginate(page: params[:page], per_page: 5)
@@ -43,5 +45,12 @@ class UsersController < ApplicationController
 
     def set_user
         @user = User.find(params[:id])
+    end
+
+    def require_same_user
+        if current_user != @user
+            flash[:alert] = "Edit permission denied"
+            redirect_to @user
+        end
     end
 end
