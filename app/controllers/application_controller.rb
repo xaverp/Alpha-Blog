6f150a1a88fcd6d0b099a103c4f7c3bbf9f71448
xaverp <xaverp@iiw.org>
2022-10-04 14:57:52 +0000

    Added user permissions constraints

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,18 @@ class ApplicationController < ActionController::Base
     helper_method :current_user, :logged_in? #THIS MAKES CURRENT_USER and LOGGED_IN? AVAILABLE TO OUR VIEW AS WELL
 
     def current_user
-        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+        @current_user ||= User.find(session[:user_id]) if session[:user_id] #x || x = y If x evaluates to nil or false then x is assigned the value of y, and left unchanged otherwise.
     end
 
     def logged_in?
         !!current_user        
+    end
+
+    def require_user
+        if !logged_in?
+            flash[:alert] = "You must be logged in to perform that action"
+            redirect_to login_path
+        end
     end
 
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
     before_action :set_article, only: [:show, :edit, :update, :destroy]
+    before_action :require_user, except: [:show, :index]
+    before_action :require_same_user, only: [:edit, :update, :destroy] #THESE LINES MUST BE IN THIS ORDER
 
     def show
     end
@@ -50,6 +52,13 @@ class ArticlesController < ApplicationController
 
     def article_params
         params.require(:article).permit(:title, :description)
+    end
+
+    def require_same_user
+        if current_user != @article.user
+            flash[:alert] = "Edit permissions denied"
+            redirect_to @article
+        end    
     end
 
 end 
